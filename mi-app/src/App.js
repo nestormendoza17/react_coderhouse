2a88bb91ec2d602203b0711855506ecc3af28bc6
Néstor Mendoza <nestor.mendoza17@yahoo.com.ar>
2022-08-24 12:12:52 -0300

    optimizando

---
+++ mi-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './asset/Header';
+import Footer from './asset/Footer';
+
+function App() {
+  return (
+    <div className='container-fluid fondo_main'>
+      <Header />
+      <hr />
+      <h1>Diablo Vinos</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ mi-app/src/asset/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./images/logo_large.png";
+
+const Header = () => {
+    return (
+     <div className="container">
+      <ul className="nav">
+        <li className="nav-item">
+         <a className="nav-link active" aria-current="page" href="#"><img src={logo} width="68"alt="logo" /></a>
+        </li>
+        <li className="nav-item">
+         <a className="nav-link link_header" href="#">Historia</a>
+        </li>
+        <li className="nav-item">
+         <a className="nav-link link_header" href="#">Promos</a>
+        </li>
+        <li className="nav-item">
+         <a className="nav-link link_header" href="#">Contacto</a>
+        </li>
+      </ul>   
+    </div>
+    )
+}
+
+export default Header;

f4eb2c7616378deab80f9c7e907a10fb7b7fbaa4
Néstor Mendoza <nestor.mendoza17@yahoo.com.ar>
2022-09-04 21:37:46 -0300

    subiendo rama master

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {

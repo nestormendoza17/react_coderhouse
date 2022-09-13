2a6d9b9e47b2e1d59684ac525fc998bf16dd1f5b
Néstor Mendoza <nestor.mendoza17@yahoo.com.ar>
2022-09-12 23:32:26 -0300

    detalles

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -2,13 +2,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   return (
     <div className="container-fluid fondo_main">
       <NavBar />
       <hr />
-      <ItemListContainer greeting="Vinos Diablo"/>
+      <ItemDetailContainer />
     </div>
   );
 }

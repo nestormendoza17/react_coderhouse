e59ae976f20f4095c40e51a0ecc2f56cc982e1ac
Néstor Mendoza <nestor.mendoza17@yahoo.com.ar>
2022-09-14 14:23:24 -0300

    correciones detalle

--- mi-app/src/components/ItemDetail.jsx
+++ mi-app/src/components/ItemDetail.jsx
@@ -1,1 +1,16 @@
 import React from "react";
+
+const ItemDetail = ({data}) => {
+    return (
+        <div clasName="contaniner">
+            <div className="detail">
+                <img clasName='detail_image' src={data.image} alt="" />
+                <div clasName="content">
+                    <h1>{data.titulo}</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ItemDetail;
